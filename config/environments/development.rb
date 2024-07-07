53378d442cefaa2a7741fd06463fedf8ba3efa2c
Zack Spalding <spalding@senylrc.org>
2024-07-07 14:14:29 -0400

    local change sync

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
 config.hosts << "beta.empireadc.org"
 config.hosts << "beta.empireadc.org:3000"
   # In the development environment your application's code is reloaded any time
