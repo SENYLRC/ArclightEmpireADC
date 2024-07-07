f34405052238e4b9dca1ef88e9e7eb00a0ba19e1
Zack Spalding <spalding@senylrc.org>
2024-07-07 14:14:00 -0400

    fix a bug where more in facet was not working

--- app/components/arclight/search_bar_component.rb
+++ app/components/arclight/search_bar_component.rb
@@ -34,25 +34,31 @@ module Arclight
                            helpers.current_context_document&.collection_name
     end
 
-    def repository_name
-      if controller.controller_name == "repositories" && controller.action_name == "show"
-          @repository_name ||= Repository.find_by!(slug: params[:id]).name
-      else
-     #This was used when viewing the EAD page to get the Repo Name for the drop down
+def repository_name
+  if controller.controller_name == "repositories" && controller.action_name == "show"
+    @repository_name ||= Repository.find_by!(slug: params[:id]).name
+  else
+    # This was used when viewing the EAD page to get the Repo Name for the drop down
     solr_url = URI("http://localhost:8983/solr/blacklight-core/select?q=id:#{params[:id]}")
     response = Net::HTTP.get(solr_url)
     solr_data = JSON.parse(response)
-    @repository_name = solr_data['response']['docs'].first['repository_ssm']
 
-    if @repository_name.is_a?(Array)
-      # Extract the first element from the array (if it exists)
-      @repository_name = @repository_name.first
-    end
-
-    # Remove the starting and trailing square brackets if present
-    @repository_name = @repository_name[0..-1] if @repository_name.is_a?(String)
+    if solr_data['response'].present? && solr_data['response']['docs'].present? && solr_data['response']['docs'].first.present?
+      @repository_name = solr_data['response']['docs'].first['repository_ssm']
+      
+      if @repository_name.is_a?(Array)
+        # Extract the first element from the array (if it exists)
+        @repository_name = @repository_name.first
       end
+
+      # Remove the starting and trailing square brackets if present
+      @repository_name = @repository_name[0..-1] if @repository_name.is_a?(String)
+    else
+      Rails.logger.error "Solr response does not contain the expected data: #{solr_data.inspect}"
+      @repository_name = nil
     end
+  end
+end
 
     def selected_option(options)
       options.detect { |option| option.last.present? }&.last || ''
