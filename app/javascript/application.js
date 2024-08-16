e17924d8dc60cc9cbd161d1eccdac2e07e076577
Zack Spalding <spalding@senylrc.org>
2024-08-16 11:50:52 -0400

    this remove dash from search pop up so recommended searches don't break

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,26 @@ import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
 
 import "arclight"
+
+// Remove dashes from the autocomplete options
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.nodeType === Node.ELEMENT_NODE && node.matches('li[role="option"]')) {
+                        const span = node.querySelector('span');
+                        if (span) {
+                            span.textContent = span.textContent.replace(/-/g, ' ');
+                        }
+                    }
+                });
+            }
+        });
+    });
+
+    const targetNode = document.getElementById('autocomplete-popup');
+    if (targetNode) {
+        observer.observe(targetNode, { childList: true, subtree: true });
+    }
+});
