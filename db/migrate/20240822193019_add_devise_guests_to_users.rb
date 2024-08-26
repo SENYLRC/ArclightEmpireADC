fc5313e7fd9dfbf16330ba3c15b5c93274d874ef
Zack Spalding <spalding@senylrc.org>
2024-08-26 16:08:09 -0400

    local syc

--- db/migrate/20240822193019_add_devise_guests_to_users.rb
+++ db/migrate/20240822193019_add_devise_guests_to_users.rb
@@ -1,4 +1,4 @@
-class AddDeviseGuestsToUsers < ActiveRecord::Migration[7.1]
+class AddDeviseGuestsToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
