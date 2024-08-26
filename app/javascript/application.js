69743e91d01cb85225b46ae21dbf96a952bd72fd
Zack Spalding <spalding@senylrc.org>
2024-08-26 14:39:38 -0400

    changes to js so they load all the time just not on the inital page load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
-
 import "arclight"
 
-// Remove dashes from the autocomplete options
-document.addEventListener('DOMContentLoaded', function() {
+function initializeAutocompleteObserver() {
     const observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
@@ -28,4 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if (targetNode) {
         observer.observe(targetNode, { childList: true, subtree: true });
     }
-});
+}
+
+// Run the function when the page is initially loaded
+document.addEventListener('DOMContentLoaded', initializeAutocompleteObserver);
+
+// Run the function on every Turbo visit
+document.addEventListener('turbo:load', initializeAutocompleteObserver);
